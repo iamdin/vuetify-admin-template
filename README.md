2f578b5cc442af23f32bda3482703594fa54af82
dingjinyang <djy.dingjinyang@gmail.com>
2020-01-30 13:41:40 +0800

    基于Google Contracts预置布局修改，完成页面基础布局

--- README.md
+++ README.md
@@ -1,24 +1,15 @@
 # vuetify-admin-template
 
-## Project setup
-```
-npm install
-```
+> 基于Vue.js的组件库Vuetify构建的前端管理系统模板，使用 Vue.js + Vue Router + Vuex，第三方库有：Vuetify & Axios & Mock & ESlint
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+项目基于vue@2.6.10，vue-router@3.1.3，vuex@3.1.2，@vue/cli 4.1.2构建
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## 为什么选用Vuetify
 
-### Lints and fixes files
-```
-npm run lint
-```
+1. Vuetify 基于 [Material Design](https://material.io/guidelines/)设计，个人感觉比较好看
+2. 丰富的组件库（82+）和长期支持
+3. Vuetify在组件设计上做了很多默认配置，省去很多工作，组件属性上也高度自定义
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## 基本功能
+
+- 自适应布局，编码时尽可能的考虑PC端和移动端的布局与展示，提高用户体验
--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,126 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      mobile-break-point="600"
+      :expand-on-hover="$vuetify.breakpoint.smOnly"
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row v-if="item.heading" :key="item.heading" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-group
+            v-else-if="item.children"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ item.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item v-for="(child, i) in item.children" :key="i" link>
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ child.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item v-else :key="item.text" link>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+    <v-app-bar app color="blue darken-3" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title class="ml-0 pl-4">
+        <span class="hidden-xs-only">Vuetify-Admin-Template</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      <v-btn icon large>
+        <v-avatar size="32px" item>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+        /></v-avatar>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <v-container fluid>
+        <h1>Vuetify-Admin-Template</h1>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
-    //
+    drawer: null,
+    items: [
+      { icon: "mdi-contacts", text: "Contacts" },
+      { icon: "mdi-history", text: "Frequently contacted" },
+      { icon: "mdi-content-copy", text: "Duplicates" },
+      {
+        icon: "mdi-chevron-up",
+        "icon-alt": "mdi-chevron-down",
+        text: "Labels",
+        model: true,
+        children: [{ icon: "mdi-plus", text: "Create label" }]
+      },
+      {
+        icon: "mdi-chevron-up",
+        "icon-alt": "mdi-chevron-down",
+        text: "More",
+        model: false,
+        children: [
+          { text: "Import" },
+          { text: "Export" },
+          { text: "Print" },
+          { text: "Undo changes" },
+          { text: "Other contacts" }
+        ]
+      },
+      { icon: "mdi-settings", text: "Settings" },
+      { icon: "mdi-message", text: "Send feedback" },
+      { icon: "mdi-help-circle", text: "Help" },
+      { icon: "mdi-cellphone-link", text: "App downloads" },
+      { icon: "mdi-keyboard", text: "Go to the old version" }
+    ]
   })
 };
 </script>
