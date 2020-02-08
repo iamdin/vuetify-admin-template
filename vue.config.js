0d10f28098d794d608545db6316671fd9dc393fb
dingjinyang <djy.dingjinyang@gmail.com>
2020-02-08 19:25:19 +0800

    delete prefetch

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   // 文件打包后 在index.html中引用的位置，与 process.env.BASE_URL相同
   publicPath:
     process.env.NODE_ENV === "production" ? "/vuetify-admin-template/" : "/",
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  chainWebpack: config => {
+    config.plugins.delete("prefetch");
+  }
 };
