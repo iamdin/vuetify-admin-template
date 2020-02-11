a73683214e8efa8a5ebdde915d7f46c6007a1339
dingjinyang <djy.dingjinyang@gmail.com>
2020-02-11 16:46:18 +0800

    random roles;

--- src/mock/user.js
+++ src/mock/user.js
@@ -1,4 +1,11 @@
 const Mock = require("mockjs");
+const Random = Mock.Random;
+Random.extend({
+  roles: function() {
+    const roles = ["root", "admin", "user"];
+    return this.pick(roles);
+  }
+});
 export default {
   /** 用户登陆 */
   login: Mock.mock("/login", {
@@ -11,7 +18,7 @@ export default {
   getUserInfo: Mock.mock("/userInfo", {
     code: 200,
     data: {
-      roles: ["root"],
+      roles: ["@ROLES"],
       name: "@NAME",
       avatar:
         // "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif"
